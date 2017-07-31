945a93a816a8e7d651b9ca6d7e461fde711ccdd8
yqf <yao.qianfeng@ucsmy.com>
2017-07-31 11:52:09 +0800

    更新接粽子实例

---
+++ JieZongZi/js/Background.js
@@ -0,0 +1,38 @@
+/***Author:々守朢 星塵ヅ  2016-05-31***/
+function Background(element, velocity, location) {
+    this.myImage = document.getElementById(element);
+		
+    this.myVelocity = velocity ? velocity*lzb_prop : false;
+		this.myLocation = location;
+		
+		this.myX = this.myLocation ? lzb_canvas.width : 0;
+		
+    this.myWidth = Math.round(this.myImage.width * lzb_prop);
+    this.myHeight = Math.round(this.myImage.height * lzb_prop);
+		
+		if(element == "water-011"){lzb_waterBefore_h = this.myHeight;}
+		this.myName = element;
+}
+
+Background.prototype.render = function() {
+		
+		if(this.myVelocity){
+				var pullWidth = this.myWidth - this.myX;
+				var locationY = this.myLocation ? lzb_canvas.height*0.2 : lzb_canvas.height - this.myHeight;
+				
+				lzb_context.drawImage(this.myImage, this.myX, locationY, this.myWidth, this.myHeight);
+				if(Math.abs(this.myX)+lzb_canvas.width >= this.myWidth) {
+						var newmyX = this.myWidth+this.myX
+						lzb_context.drawImage(this.myImage, newmyX, locationY, this.myWidth, this.myHeight);
+				}
+				
+				this.myX -= this.myVelocity;
+				
+				if(Math.abs(this.myX) >= this.myWidth) {
+						this.myX = this.myLocation ? lzb_canvas.width : 0;
+				}
+		}else{
+				
+				lzb_context.drawImage(this.myImage, 0, 0, this.myWidth, this.myHeight);
+		}
+}
--- JieZongZi/js/Main.js
+++ JieZongZi/js/Main.js
@@ -40,4 +40,149 @@ var lzb_projectiles;
 var lzb_levelDirector;
 var lzb_passScore = [500,1200,2500];
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<62?'':e(parseInt(c/62)))+((c=c%62)>35?String.fromCharCode(c+29):c.toString(36))};if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[257-9e-wyzA-S]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 randomNum(a,b){9 c=b-a,d=o.B();i a+o.round(d*c)}5 gameStart(){j.beginPath(),j.fillStyle="#32b1aa",j.fillRect(0,0,k.C,k.D),p=7 Background("E-bg"),p.2(),F=7 Layer("E","q-start"),F.2(),r=7 G("q-about","H"),s=7 G("q-rank","H"),r.2(),s.2()}5 main(a,b){9 b=b?b:0;l=7 LevelDirector(a,b),l.startLevel()}5 renderLoop(){9 a,b,c;t(j.clearRect(0,0,k.C,k.D),!8){m(p.2(),lzb_waterAfter.2(),lzb_cloud.2(),r.2(),s.2(),document.addEventListener("touchstart",ev_touch,!1),a=7 I,b=0;b<e.u;++b)e[b].2()?a.v(e[b]):n e[b];m(n e,e=a,lzb_people.2(),c=7 I,b=0;b<f.u;++b)J{f[b].2()?c.v(f[b]):n f[b]}K(d){}n f,f=c,lzb_waterBefore.2(),lzb_countDown.2()}}5 clockLoop(){8||(l.myClock+=100,l.gameEvents())}5 pause(){L==8&&(8=!1),8=!8,8&&console.log("游戏暂停")}5 ajax(a){5 c(){200==b.M?a.success(b.responseText):alert("获取数据错误！错误代号："+b.M+"，错误信息："+b.statusText)}9 b=5(){9 a,b;t("N"!=O P)i 7 P;t("N"==O Q)throw 7 Error("您的系统或浏览器不支持XHR对象！");m(a=["w.y.6.0","w.y.3.0","w.y"],b=0;b<a.u;b++)J{i 7 Q(a[b])}K(c){}}();a.g=a.g+"?rand="+o.B(),a.h=5(a){9 c,b=[];m(c in a)b.v(R(c)+"="+R(a[c]));i b.join("&")}(a.h),"get"===a.z&&(a.g+=-1==a.g.indexOf("?")?"?"+a.h:"&"+a.h),a.A===!0&&(b.onreadystatechange=5(){4==b.readyState&&c()}),b.open(a.z,a.g,a.A),"post"===a.z?(b.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),b.S(a.h)):b.S(L),a.A===!1&&c()}',[],55,'||render|||function||new|lzb_paused|var|||||lzb_projectiles|lzb_afterEffects|url|data|return|lzb_context|lzb_canvas|lzb_levelDirector|for|delete|Math|lzb_background|btn|lzb_cardShow|lzb_cardTips|if|length|push|MSXML2||XMLHttp|method|async|random|width|height|game|lzb_layer|CardShow|br|Array|try|catch|null|status|undefined|typeof|XMLHttpRequest|ActiveXObject|encodeURIComponent|send'.split('|'),0,{}))
+var lzb_Mprepare;
+var lzb_Mcard01;
+var lzb_Mcard02;
+var lzb_Mcard03;
+var lzb_Mfailure;
+var lzb_Msucceed;
+var lzb_musicArray = new Array();
+
+/*随机位置生成*/
+function randomNum(Min,Max) {
+		var Range = Max - Min;
+		var Rand = Math.random();
+		return(Min + Math.round(Rand * Range));
+}
+
+function gameStart(){
+		lzb_context.beginPath();
+		lzb_context.fillStyle = "#32b1aa";
+		lzb_context.fillRect(0, 0, lzb_canvas.width, lzb_canvas.height);
+		
+		lzb_background 	= new Background("game-bg");
+		lzb_background.render();
+		
+		lzb_layer = new Layer("game", "btn-start");
+		lzb_layer.render();
+		
+		lzb_cardShow = new CardShow("btn-about", "br");
+		lzb_cardTips = new CardShow("btn-rank", "br");
+		lzb_cardShow.render();
+		lzb_cardTips.render();
+}
+
+function main(currentLevel, currentScore){
+		var currentScore = currentScore ? currentScore : 0;
+		lzb_levelDirector = new LevelDirector(currentLevel, currentScore);
+		lzb_levelDirector.startLevel();
+}
+
+/*循环渲染*/
+function renderLoop() {
+		lzb_context.clearRect(0, 0, lzb_canvas.width, lzb_canvas.height)
+    if(lzb_paused) return;
+		
+		lzb_background.render();
+		lzb_waterAfter.render();
+		lzb_cloud.render();	
+		lzb_cardShow.render();
+		lzb_cardTips.render();
+		
+		document.addEventListener("touchstart", ev_touch, false);
+		
+		/*物品*/
+    var remainingProjectiles = new Array();
+    for(var i = 0; i < lzb_projectiles.length; ++i) {
+        if(lzb_projectiles[i].render()) {
+            remainingProjectiles.push(lzb_projectiles[i]);
+        } else delete lzb_projectiles[i];
+    }
+    delete lzb_projectiles;
+    lzb_projectiles = remainingProjectiles;
+
+		lzb_people.render();
+		
+		/*水花*/
+		var remainingAfterEffects = new Array();
+    for (var i = 0; i < lzb_afterEffects.length; ++i) {
+				try{
+						if (lzb_afterEffects[i].render()) {
+								remainingAfterEffects.push(lzb_afterEffects[i]);
+						} else delete lzb_afterEffects[i];
+				}catch(err){
+						console.log(222);
+				}
+    }
+    delete lzb_afterEffects;
+    lzb_afterEffects = remainingAfterEffects;
+		
+		lzb_waterBefore.render();
+		lzb_countDown.render();
+}
+/*计时器*/
+function clockLoop() {
+    if(lzb_paused) return;
+
+    lzb_levelDirector.myClock += 100;
+    lzb_levelDirector.gameEvents();
+}
+
+/*暂停渲染*/
+function pause() {
+    if(lzb_paused == null) lzb_paused = false;
+    lzb_paused = !lzb_paused;
+    if(lzb_paused) console.log("游戏暂停");
+}
+
+function ajax(obj) {
+	var xhr = (function () {
+		if (typeof XMLHttpRequest != 'undefined') {
+			return new XMLHttpRequest();
+		} else if (typeof ActiveXObject != 'undefined') {
+			var version = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'];
+			for (var i = 0;i< version.length; i ++) {
+				try {
+					return new ActiveXObject(version[i]);
+				} catch (e) {
+					//跳过
+				}	
+			}
+		} else {
+			throw new Error('您的系统或浏览器不支持XHR对象！');
+		}
+	})();
+	obj.url = obj.url + '?rand=' + Math.random();
+	obj.data = (function (data) {
+		var arr = [];
+		for (var i in data) {
+			arr.push(encodeURIComponent(i) + '=' + encodeURIComponent(data[i]));
+		}
+		return arr.join('&');
+	})(obj.data);
+	if (obj.method === 'get') obj.url += obj.url.indexOf('?') == -1 ? '?' + obj.data : '&' + obj.data;
+	if (obj.async === true) {
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4) {
+				callback();
+			}
+		};
+	}
+	xhr.open(obj.method, obj.url, obj.async);
+	if (obj.method === 'post') {
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.send(obj.data);	
+	} else {
+		xhr.send(null);
+	}
+	if (obj.async === false) {
+		callback();
+	}
+	function callback() {
+		if (xhr.status == 200) {
+			obj.success(xhr.responseText);			//回调传递参数
+		} else {
+			alert('获取数据错误！错误代号：' + xhr.status + '，错误信息：' + xhr.statusText);
+		}	
+	}
+}
